37c5289fb301bf1106eb24ae061e4ac0388cce6b
thaemilly22 <163013581+thaemilly22@users.noreply.github.com>
2024-10-10 11:29:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'Desenho',
+    'Qual é o desenho que tem um gato e um rato?',
+    'Tom e Jerry'
+)
+
+criaCartao(
+    'Desenho',
+    'Qual é o gato que gosta de comer lasanha?',
+    'Garfield'
+)
+
+criaCartao(
+    'Filme',
+    'O melhor amigo da vizinhança?',
+    'Homem Aranha'
+)
+
+criaCartao(
+    'Filme',
+     'Quem roubou o coração de tefite?',
+    'Maui'
+)
+criaCartao(
+    'Filme',
+    'Por que os minions virou o rei da inglaterra?',
+    'Tirou a espada da pedra'
+)
+
+criaCartao(
+    'Filme',
+    'Qual o nome do filme que o rato controla o cozinheiro?',
+    'ratatuille'
+)
